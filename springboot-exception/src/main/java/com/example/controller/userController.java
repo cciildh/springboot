53c64fa9e992dba1135b0a295042583d8940acbb
ldhMac <493830938@qq.com>
2020-08-14 00:26:19 +0800

    全局异常

--- springboot-exception/src/main/java/com/example/controller/userController.java
+++ springboot-exception/src/main/java/com/example/controller/userController.java
@@ -1,10 +1,14 @@
 package com.example.controller;
 
+import javax.validation.constraints.Min;
+
 import com.example.entity.StdEmp;
 import com.example.service.StdEmpservice;
 import com.example.utils.ResponseMesg;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -26,12 +30,17 @@ public class userController {
 
     @RequestMapping("/add")
     @ResponseBody
-    public ResponseMesg adduser(StdEmp emp) {
+    public ResponseMesg adduser(@Validated StdEmp  emp ,BindingResult bindingResult) {
 
         int rows = empserviceimpl.Save(emp);
         if (rows > 0) {
             return ResponseMesg.ok();
         }
         return ResponseMesg.errorMsg("未知错误！");
+    }
+    @RequestMapping("/getemp")
+    @ResponseBody
+    public ResponseMesg geteMp(@Min(value = 1,message = "id不能为空!") int id){
+        return ResponseMesg.ok();
     }
 }
